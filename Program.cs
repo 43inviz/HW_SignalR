ccf00dab8fe3e849293632f31b36b9393aa20a99
Max Teplov <153938230+43inviz@users.noreply.github.com>
2024-11-27 16:15:15 +0100

    signalR

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace HW_signalR;
+
+/*internal class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var connection = new HubConnectionBuilder().WithUrl("https://localhost:7012//currencyHub").Build();
+
+        connection.On<string, decimal>("ReceiveCurrencyUpdate", (currencyPair, rate) =>
+        {
+            Console.WriteLine($"New rate for {currencyPair}: {rate}");
+
+
+        });
+
+        await connection.StartAsync();
+        Console.WriteLine("Connect to server.");
+        Console.ReadLine();
+    }
+
+    
+}*/
+
